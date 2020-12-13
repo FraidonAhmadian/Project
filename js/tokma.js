8ded2d5199cff5bbc755406b6853ff877dbd35c6
FraidonAhmadian <feraidonahmadian2016@gmail.com>
2020-12-13 22:09:24 +0430

    First commit

---
+++ js/tokma.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$(".header-links").hover(function(){
+	  $("#option-hover").show();
+	});
+	
+	$(".header-links").hover(function(){
+	  $("#option-hover").show();
+	});
+	
+	$(".header-links").mouseleave(function(){
+	  $("#option-hover").hide();
+	});
+	
+   $("#option-hover").hover(function(){
+	   this.show();
+   });
+});
